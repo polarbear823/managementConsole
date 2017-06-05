788abe0d6e7900fbfec386415810552f7e5cd6cd
polarbear823 <xtinezhu@hotmail.com>
2017-06-04 22:35:44 -0700

    add login page detail: value change methods

--- src/components/login.js
+++ src/components/login.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const Login = () => {
-	return (
-		<div className="container">
-			<form className="form-signin">
-				<h2 className="form-signin-heading">Please sign in</h2>
-				<label htmlFor="inputEmail" className="sr-only">Email address</label>
-				<input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-				<label htmlFor="inputPassword" className="sr-only">Password</label>
-				<input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-				<div className="checkbox">
-					<label>
-						<input type="checkbox" value="remember-me" /> Remember me
-					</label>
-				</div>
-				<button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-			</form>
-		</div>
+class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			isRememberMe: false
+		};
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onEmailValueChange = this.onEmailValueChange.bind(this);
+		this.onPasswordValueChange = this.onPasswordValueChange.bind(this);
+		this.onRememberMeChange = this.onRememberMeChange.bind(this);
+	}
+	render(){
+		return (
+			<div className="container">
+				<form className="form-signin" onSubmit={this.onSubmit}>
+					<h2 className="form-signin-heading">Please sign in</h2>
+					<label htmlFor="inputEmail" className="sr-only">Email address</label>
+					<input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus onChange={this.onEmailValueChange}/>
+					<label htmlFor="inputPassword" className="sr-only">Password</label>
+					<input type="password" id="inputPassword" className="form-control" placeholder="Password" required onChange={this.onPasswordValueChange}/>
+					<div className="checkbox">
+						<label>
+							<input type="checkbox" value="remember-me" checked={this.state.isRememberMe} onChange={this.onRememberMeChange}/> Remember me
+						</label>
+					</div>
+					<button className="btn btn-lg btn-primary btn-block" type="submit" onSub>Sign in</button>
+				</form>
+			</div>
 		);
+	}
+
+	onSubmit(e) {
+		console.log(this.state);
+		e.preventDefault();
+	}
+
+	onEmailValueChange(e) {
+		this.setState({email: e.target.value});
+	}
+
+	onPasswordValueChange(e) {
+		this.setState({password: e.target.value});
+	}
+
+	onRememberMeChange(e) {
+		this.setState({isRememberMe: !this.state.isRememberMe});
+	}
 };
 export default Login;
